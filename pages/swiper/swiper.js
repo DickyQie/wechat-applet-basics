9a747c220ec2ddc1a6bf3651430ff3091c75a4bc
DickyQie <18523978916@163.com>
2017-09-02 12:39:37 +0800

    微信小程序------轮播图

---
+++ pages/swiper/swiper.js
@@ -0,0 +1,71 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrl: [
+        'https://a.vimage1.com/upload/brand/upcb/2017/08/11/97/01006302_604x290_80.jpg',
+        'https://a.vimage1.com/upload/brand/upcb/2017/08/17/153/ias_150293567439328_604x290_80.jpg',
+        'https://a.vimage1.com/upload/brand/upcb/2017/08/15/53/ias_150276254071689_604x290_80.jpg',
+        'https://a.vimage1.com/upload/brand/upcb/2017/08/11/154/ias_150244675261299_604x290_80.jpg'
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
